bacd847c1ed819728222f075cb079d3e406294a9
Dominik Leitner <leitner.dominik@hotmail.de>
2026-06-30 10:09:42 +0200

    centralize maze reset state

--- src/sketch.js
+++ src/sketch.js
@@ -1,27 +1,42 @@
 const cols = 20;
 const rows = 20;
-const grid = [];
-let isMazeCompleted = false;
+const APP_STATUS = Object.freeze({
+    GENERATING: 'generating',
+    SOLVING: 'solving',
+    SOLVED: 'solved',
+    NO_SOLUTION: 'no-solution',
+    PAUSED: 'paused',
+});
 
+let grid = [];
+let appStatus = APP_STATUS.GENERATING;
 let recursiveBacktracker,
-    astar;
+    astar,
+    startCell,
+    goalCell;
 
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    resetMaze();
+}
 
+function createGrid() {
     const cellWidth = Math.floor(width / cols);
     const cellHeight = Math.floor(height / rows);
+    const nextGrid = [];
 
     for(let i = 0; i < cols; i++) {
-        grid[i] = new Array(rows);
+        nextGrid[i] = new Array(rows);
     }
 
     for(let x = 0; x < cols; x++) {
         for(let y = 0; y < rows; y++) {
-            grid[x][y] = new Cell(x, y, cellWidth, cellHeight);
+            nextGrid[x][y] = new Cell(x, y, cellWidth, cellHeight);
         }
     }
+
+    grid = nextGrid;
 
     for(let x = 0; x < cols; x++) {
         for(let y = 0; y < rows; y++) {
@@ -29,12 +44,21 @@ function setup() {
         }
     }
 
-    // frameRate(10);
+    return {
+        grid: grid,
+        startCell: grid[0][0],
+        goalCell: grid[cols - 1][rows - 1],
+    };
+}
 
+function resetMaze() {
+    const maze = createGrid();
+    startCell = maze.startCell;
+    goalCell = maze.goalCell;
     recursiveBacktracker = new RecursiveBacktracker(grid);
-    const startCell = grid[0][0];
-    const goalCell = grid[cols-1][rows-1];
     astar = new AStar(startCell, goalCell);
+    appStatus = APP_STATUS.GENERATING;
+    loop();
 }
 
 function draw() {
@@ -46,13 +70,21 @@ function draw() {
         }
     }
 
-    if(!isMazeCompleted) {
-        isMazeCompleted = recursiveBacktracker.update();
-    }
-    else {
-        let solution = astar.search();
+    if(appStatus === APP_STATUS.GENERATING) {
+        const generationComplete = recursiveBacktracker.update();
+        if(generationComplete) {
+            appStatus = APP_STATUS.SOLVING;
+        }
+    } else if(appStatus === APP_STATUS.SOLVING) {
+        const solution = astar.search();
         astar.drawPath();
 
-        if(solution != undefined) noLoop();
+        if(solution === true) {
+            appStatus = APP_STATUS.SOLVED;
+            noLoop();
+        } else if(solution === false) {
+            appStatus = APP_STATUS.NO_SOLUTION;
+            noLoop();
+        }
     }
 }
